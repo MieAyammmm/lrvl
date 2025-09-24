0ecb10986d5a11eedeb88cffd85eaa65813bd7be
dani <dhannizd2@gmail.com>
2025-09-24 13:03:49 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// CRUD Routes untuk Users
+Route::resource('users', UserController::class);
